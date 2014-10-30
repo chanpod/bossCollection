c46c89a7630e13d8c0562410e5ffe4037778589b
chanpod <chanpod36@gmail.com>
2014-10-29 22:32:51 -0500

    maybe working?

--- sqlView/sqlController.js
+++ sqlView/sqlController.js
@@ -23,17 +23,14 @@ angular.module('myApp.sqlView', ['ngRoute'])
 	$scope.queryTableData = [];
 	$scope.queryColumns = [];
 
-        function jsonp_callback(data) {
-            // returning from async callbacks is (generally) meaningless
-            console.log(data.found);
-        }
 
 $scope.getAbility = function(query) {
 
-    var url = 'http://us.api.battle.net/wow/battlePet/ability/640?locale=en_US&apikey=fqvadba9c8auw7brtdr72vv7hfntbx7d?callback=JSON_CALLBACK';
-
-    $http.jsonp(url).success(function (data) {
-        console.log(data);
+    $http({
+        url:'https://us.api.battle.net/wow/battlePet/ability/640?locale=en_US&apikey=fqvadba9c8auw7brtdr72vv7hfntbx7d',
+        method: "GET"
+    }).success(function (data) {
+        $scope.users = data;
     });
 
 }
