39568fbd58718099c6af828ff550fcfc29ba6e30
chanpod <chanpod36@hotmail.com>
2014-10-30 08:45:10 -0500

    attempting to fix api call

--- sqlView/sqlController.js
+++ sqlView/sqlController.js
@@ -26,7 +26,7 @@ angular.module('myApp.sqlView', ['ngRoute'])
 
 $scope.getAbility = function(query) {
     var url = 'https://us.api.battle.net/wow/battlePet/ability/640?locale=en_US&apikey=fqvadba9c8auw7brtdr72vv7hfntbx7d&jsonp=JSON_CALLBACK';
-    $http.jsonp().success(function (data) {
+    $http.jsonp(url).success(function (data) {
         $scope.users = data;
     });
 
