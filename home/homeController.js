58bef8fbfb972dc857e3163050f0f21aa7438c1d
chanpod <chanpod36@gmail.com>
2014-10-30 21:18:56 -0500

    maybe working?

--- home/homeController.js
+++ home/homeController.js
@@ -13,15 +13,17 @@ angular.module('myApp.home', ['ngRoute'])
 	var apiKey = "apikey=fqvadba9c8auw7brtdr72vv7hfntbx7d";
     var jsonP = "jsonp=JSON_CALLBACK";
     var blizzApiRoot = "https://us.api.battle.net/wow/";
-    var charInfo = "character/Zul'Jin/Defragmentor";
+    var charInfo = "character/" + $scope.realm = "/" + $scope.charName;
+    var guildInfo = ""
     var staticResources = "http://us.battle.net/static-render/us/";
 
     $scope.classImg = "";
     $scope.guild = "";
     $scope.realm = "";
+    $scope.charName = "";
 
 
-$scope.getAbility = function() {
+$scope.getCharacter = function() {
     var url = blizzApiRoot + charInfo + '?locale=en_US&' + apiKey + '&' + jsonP;
     $http.jsonp(url).success(function (data) {
         console.log(data);
