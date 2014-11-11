a4cafe474158129af4d3d23d5a871726aa29d62b
chanpod <chanpod36@gmail.com>
2014-11-11 12:56:24 -0600

    refactored the guild jade file

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -119,7 +119,7 @@ angular.module("BossCollection.controllers", ['BossCollection.services'])
 
             $scope.open = function (url) {
                 $scope.setUrl(url);
-
+                console.log(url);
                 var modalInstance = $modal.open({
                     templateUrl: 'videoModal',
                     controller: 'videoController',
@@ -132,6 +132,59 @@ angular.module("BossCollection.controllers", ['BossCollection.services'])
                     }
                 });
             }
+
+            $scope.highmaulBossInfo = {
+                "0kargath" : {
+                    "embeddedUrl" : "D5QqzwJIPa0",
+                    "name" : "Kargath Bladefist",
+                    "stratUrl" : "http://www.icy-veins.com/wow/kargath-bladefist-strategy-guide-preview",
+                    "img" : "Highmaul/kargath.png",
+                    "wowheadNPCNum" : "87444"
+                },
+                "1butcher" : {
+                    "embeddedUrl" : "QkLExQUJgW4",
+                    "name" : "The Butcher",
+                    "stratUrl" : "http://www.icy-veins.com/wow/the-butcher-strategy-guide-preview",
+                    "img" : "Highmaul/theButcher.png",
+                    "wowheadNPCNum" : "87447"
+                },
+                "2Tectus" : {
+                    "embeddedUrl" : "C0VYzWNYDF0",
+                    "name" : "Tectus",
+                    "stratUrl" : "http://www.icy-veins.com/wow/tectus-strategy-guide-preview",
+                    "img" : "Highmaul/tectus.png",
+                    "wowheadNPCNum" : "87446"
+                },
+                "3brackenspore" : {
+                    "embeddedUrl" : "mIdrE_7iqc8",
+                    "name" : "Brackenspore",
+                    "stratUrl" : "http://www.icy-veins.com/wow/brackenspore-strategy-guide-preview",
+                    "img" : "Highmaul/brackenspore.png",
+                    "wowheadNPCNum" : "87441"
+                },
+                "4twinOgron" : {
+                    "embeddedUrl" : "AJ4xo4jqJlU",
+                    "name" : "Twin Ogron",
+                    "stratUrl" : "http://www.icy-veins.com/wow/twin-ogron-strategy-guide-preview",
+                    "img" : "Highmaul/twinOgron.png",
+                    "wowheadNPCNum" : "87449"
+                },
+                "5koragh" : {
+                    "embeddedUrl" : "y9ha5_IkI7A",
+                    "name" : "Ko'ragh",
+                    "stratUrl" : "http://www.icy-veins.com/wow/ko-ragh-strategy-guide-preview",
+                    "img" : "Highmaul/ko'ragh.png",
+                    "wowheadNPCNum" : "87445"
+                },
+                "6margok" : {
+                    "embeddedUrl" : "i4OMq9fYgVY",
+                    "name" : "Imperator Mar'gok",
+                    "stratUrl" : "http://www.icy-veins.com/wow/imperator-mar-gok-strategy-guide-preview",
+                    "img" : "Highmaul/imperatorMar'gok.png",
+                    "wowheadNPCNum" : "87818"
+                }
+            }
+
             }]).controller("videoController", ['$scope', 'currentUrl',
                 function($scope, currentUrl){
 
