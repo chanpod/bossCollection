5e814b3ce13729580074ae233256d49eac3a9283
chanpod <chanpod36@hotmail.com>
2015-02-17 16:23:53 -0600

    removed a lot of crap data from services

--- public/js/services.js
+++ public/js/services.js
@@ -2,62 +2,12 @@
 
 /* Services */
 var service = angular.module("BossCollection.services", ["ngResource"]);
-var local = "locale=en_US";
-var endUrl = local + "&apikey=fqvadba9c8auw7brtdr72vv7hfntbx7d&jsonp=JSON_CALLBACK";
-var blizzApiRoot = "https://us.api.battle.net/wow/";
-var getItems = "fields=items";
-var getClasses = "data/character/classes";
-var getGuild = "fields=achievements,members";
+
 var socket = io("http://54.173.24.121:4001");
 //var socket = io("http://localhost:4001");
 
-//https://us.api.battle.net/wow/data/character/classes?locale=en_US&apikey=fqvadba9c8auw7brtdr72vv7hfntbx7d
-//https://us.api.battle.net/wow/guild/Zul'jin/crux?fields=achievements&locale=en_US&apikey=fqvadba9c8auw7brtdr72vv7hfntbx7d
-var classes = ["warrior", "paladin", "hunter", "rogue", "priest", "dk", "shaman", "mage", "warlock","monk","druid"]
+service.factory('charService', function($http, $q){
 
-service.factory('socket', function(mySocket){
-
-
-}).factory('charService', function($http, $q){
-
-        var charApi = {
-            getCharacter: function (realm, charName){
-                var deferred = $q.defer();
-
-                var charInfo = "character/" + realm + "/" + charName + "?" + getItems + ",progression,guild,talents,";
-                var url = blizzApiRoot + charInfo + '?' + local + '&' + endUrl;
-                $http.jsonp(url).success(function (data) {
-                    console.log(data);
-                    deferred.resolve(data)
-                });
-
-                return deferred.promise;
-            },
-            getClass: function(character){
-                var className = "";
-
-                classes.forEach(function(result, index){
-                    if(character.class - 1 === index){
-                        className = result;
-                    }
-                });
-
-
-
-                return className;
-            },
-            getiLvl: function(character){
-                //https://us.api.battle.net/wow/character/Zul'jin/Defragmentor?fields=items&locale=en_US&apikey=fqvadba9c8auw7brtdr72vv7hfntbx7d
-
-                    return character.items.averageItemLevelEquipped
-
-            },
-            getSpecs: function(character){
-                return character.items.averageItemLevelEquipped
-            }
-        };
-
-    return charApi;
     }).factory('guildServices', function($http, $q){
 
         var guildApi = {
@@ -66,16 +16,6 @@ service.factory('socket', function(mySocket){
             },
 
             getGuild: function(realm, guildName){
-
-                var deferred = $q.defer();
-                var url = blizzApiRoot + "/guild/" + realm + "/" + guildName + '?' + getGuild + "&" + endUrl;
-
-                $http.jsonp(url).success(function (data) {
-                    console.log(data);
-                    deferred.resolve(data)
-                });
-
-                return deferred.promise;
             }
         };
 
