c03f6317036e01ff5cfe2e7acf25cd96ec0985be
Chauncey Philpot <chanpod36@gmail.com>
2015-12-29 22:47:47 -0600

    added some models that are relevant

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose');
  
 module.exports = mongoose.model('User',{
-    username: String,
+    name: String,
     password: String,
     email: String,
-    gender: String,
-    address: String
+    battlenet: String,
+    characters: []
 });
