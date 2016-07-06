d4d8a1f88b09d569d0963dfafd375eaf33e8c559
Chauncey Philpot <chanpod36@gmail.com>
2016-07-05 20:51:54 -0500

    added item level and added a faster gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -124,6 +124,36 @@ gulp.task('concatVendor', function () {
         './node_modules/highcharts/modules/solid-gauge.js'
 
     ])
+        .pipe(concat('vendor.js'))        
+        .on('error', function (err) {
+            console.log(err);
+            this.emit('end');
+        })
+        .pipe(gulp.dest('./app/public/tmp/'))
+})
+
+gulp.task('concatVendorProduction', function () {
+
+    return gulp.src([
+        './node_modules/lodash/lodash.min.js',
+        './node_modules/angular/angular.min.js',
+        './node_modules/angular-cookies/angular-cookies.min.js',
+        './node_modules/angular-resource/angular-resource.min.js',
+        './node_modules/angular-route/angular-route.min.js',
+        './node_modules/angular-animate/angular-animate.min.js',
+        './node_modules/angular-messages/angular-messages.min.js',
+        './node_modules/angular-aria/angular-aria.min.js',
+        './node_modules/showdown/dist/showdown.min.js',
+        './node_modules/angular-material/angular-material.min.js',
+        './node_modules/moment/min/moment.min.js',
+        './node_modules/highcharts/highcharts.js',
+        './node_modules/highcharts/highcharts-more.js',
+        './node_modules/highcharts/modules/data.js',
+        './node_modules/highcharts/modules/drilldown.js',
+        './node_modules/highcharts/modules/exporting.js',
+        './node_modules/highcharts/modules/solid-gauge.js'
+
+    ])
         .pipe(concat('vendor.js'))
         .pipe(minify())
         .on('error', function (err) {
