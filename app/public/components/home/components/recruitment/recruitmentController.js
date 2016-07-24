759b3bef5974d9d13f715733f9e91e5c06cf0440
Chauncey Philpot <chanpod36@gmail.com>
2016-07-23 23:05:13 -0500

    added the initial recruitment look and feel

---
+++ app/public/components/home/components/recruitment/recruitmentController.js
@@ -0,0 +1,9 @@
+
+angular.module("BossCollection.home")
+    .controller("recruitmentController", ["$scope", '$location', '$http', '$timeout', 'siteServices', 'guildServices', 'userLoginSrvc',
+        function ($scope, $location, $http, $timeout, siteServices, guildServices, userLoginSrvc) {
+             
+             console.log("Get recruitment object");             
+
+        }])
+  
